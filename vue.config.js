2760121464679512722674ee10f6f39c8cf0ec50
alex <alextmn1@gmail.com>
2022-11-04 15:32:55 -0400

    gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/learn-spanish-with-ai/'
+  ? '/alextmn-learn-spanish-with-ai/'
   : '/',
   transpileDependencies: true
 })
